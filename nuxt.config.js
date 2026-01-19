2a6b3862ea235966655cd089cb4c63be073693fa
Richard Holzner <richard.holzner@gmail.com>
2026-01-19 23:39:48 +0100

    Add graphql / apollo

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default defineNuxtConfig({
       firebaseStorageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
       firebaseMessagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
       firebaseAppId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+      cmsGraphql: process.env.NUXT_PUBLIC_CMS_GRAPHQL,
     },
   },
 
