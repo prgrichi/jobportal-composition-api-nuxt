4a020e9a87a6f9a65e91c1c2343f6596912ca7ba
Richard Holzner <richard.holzner@gmail.com>
2026-01-22 17:54:56 +0100

    Refactor page for hygraph data

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <HeroSection />
-  <ThreeJobsTeaser />
-  <WhySection />
-  <InfoStripSection />
-  <TechStackSection />
+  <component
+    v-for="section in sections"
+    :key="section.id"
+    :is="componentMap[section.__typename]"
+    v-bind="section"
+  />
 </template>
 
 <script setup>
@@ -11,11 +12,19 @@ definePageMeta({
   name: 'home',
   title: 'Home',
 });
-import HeroSection from '@/components/home/HeroSection.vue';
-import ThreeJobsTeaser from '@/components/home/ThreeJobsTeaser.vue';
-import WhySection from '@/components/home/WhySection.vue';
-import InfoStripSection from '@/components/home/InfoStripSection.vue';
-import TechStackSection from '@/components/home/TechStackSection.vue';
+import { useI18n } from 'vue-i18n';
+import { sectionRegistry } from '../lib/sectionRegistry';
+import { usePageQuery } from '~/lib/usePageQuery';
+import { HOME_PAGE_QUERY } from '~/graphql';
+
+const { locale } = useI18n();
+const hygraphLocale = computed(() => (locale.value === 'de' ? 'de' : 'en'));
+
+const { data } = await usePageQuery('home', HOME_PAGE_QUERY, hygraphLocale);
+
+const sections = computed(() => data.value?.page?.sections ?? []);
+
+const componentMap = sectionRegistry;
 </script>
 
 <style scoped></style>
